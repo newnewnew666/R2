9e87677150721e117fb6eb290d1ab242c6006e50
伏魔大帝 <36253538+newnewnew666@users.noreply.github.com>
2018-10-19 17:19:02 +0800

    Update ToBeNumberOne.java

--- GitTest/src/com/dingdian/pakage1/ToBeNumberOne.java
+++ GitTest/src/com/dingdian/pakage1/ToBeNumberOne.java
@@ -4,10 +4,6 @@ public class ToBeNumberOne
 {
 	public static void main(String[] args)
 	{
-<<<<<<< HEAD
 		System.out.println("this is test2");
-=======
-		System.out.println("this is test0");
->>>>>>> branch 'master' of https://github.com/newnewnew666/R2.git
 	}
 }
